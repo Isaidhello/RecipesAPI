349ff2e2167950eccd957b1072740fec77f2a3d3
tsiquei <tsiquei@its.jnj.com>
2016-05-22 11:55:11 -0300

    Fixing merge error

--- app/USDA/USDAData.php
+++ app/USDA/USDAData.php
@@ -8,7 +8,6 @@ use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\RequestException;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Config;
 
 class USDAData {
 
